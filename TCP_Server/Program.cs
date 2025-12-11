f29bc2347b5426366a4a7ba7558d4e7dd59d0b6d
CAMPUSFP <marcos.palomomendez.25@campusfp.es>
2025-12-11 14:25:19 +0100

    Agregar archivos de proyecto.

---
+++ TCP_Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Servidor que escucha cualquier IP local
+            TcpListener miserverGuapo = new TcpListener(IPAddress.Any,5000);
+            //abre el puerto y empieza a escuchar conexiones entrantes
+            miserverGuapo.Start();
+            Console.WriteLine("Esperando a que se conecte mi pana...");
+
+            //AcceptTcpClient es una llamada bloqueante
+            //se queda esperando a que un cliente se conecte
+            //cuando alguien se conecta devuelve el tcpClient
+            TcpClient cliente = miserverGuapo.AcceptTcpClient();
+            Console.WriteLine("Cliente conectado");
+
+            Console.Write("Escribe lo que le quieres mandar al cliente: ");
+            string mensajito = Console.ReadLine();
+
+            //Obtener el flujo de datos asociado al socket de el clinete especifico
+            //todo lo que escriba en flujodatos se envia al cliente
+            NetworkStream flujodatos = cliente.GetStream();
+            byte[] purodatachorizo = Encoding.UTF8.GetBytes(mensajito);
+            flujodatos.Write(purodatachorizo, 0, purodatachorizo.Length);
+
+            //cierra la conexion con el cliente
+            cliente.Close();
+
+            //cierra el puerto y deha de escuchar
+            miserverGuapo.Stop();
+
+
+        }
+    }
+}
---
+++ TCP_Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("TCP_Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TCP_Server")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("b5f250d2-f2a6-46ed-9946-c9d9608a226d")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
